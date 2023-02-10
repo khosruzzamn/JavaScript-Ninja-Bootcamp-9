d960bfe4049b9a3a49b4f98bc1448399fd8b41b5
Md.khosruzzaman <khosru.skz@gmail.com>
2023-02-10 01:23:25 +0000

    Number js

---
+++ Day-04/number.js
@@ -0,0 +1,22 @@
+/*
+If you use single or Double quotes your data type turn into a string. Please do not use single or Double Quotes for Number. Write number directly.
+*/
+
+// Number Data Type
+console.log(123);
+console.log(56.202);
+
+// Doing some math using Number Data Type
+console.log(10 + 20);
+console.log(40 - 20);
+console.log(40 / 5);
+console.log(5 * 5);
+console.log(5 ** 3);
+
+// Modulus or Remainder  Operator
+console.log(10 % 2);
+console.log(100 % 7);
+
+// Check Number
+console.log(typeof 1234);
+console.log(typeof 56.202);
