6942e084ed5bcbc61929ddf1cc107dd04b2d5deb
Md.khosruzzaman <khosru.skz@gmail.com>
2023-02-10 01:26:38 +0000

    Variable js

---
+++ Day-04/String.js
@@ -0,0 +1,24 @@
+/*
+String is always bind in single or Double Quotes.
+*/
+
+// String Data Type
+console.log("Hello Bangladesh");
+console.log("Hello Bangladesh");
+
+// Check String
+console.log(typeof "Hello World");
+console.log(typeof "Hello World");
+
+// Concatenation data with "+"
+console.log("hello" + " world");
+
+// Escape Character with "\"
+console.log("hello i'm Eyahiya");
+
+// Template String for better and Cleaner output
+console.log(`Hi, i'm eyahiya `);
+
+// Measure the length of String
+console.log("Hi, This is Eyahiya Mahmud".length);
+console.log("Hi, This is Eyahiya Mahmud"[12]);
